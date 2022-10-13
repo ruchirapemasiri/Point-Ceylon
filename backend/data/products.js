46450eba3903288d035f7e0740f71bdabac5d547
ruchirapemasiri <KADSE201F007@student.nibm.lk>
2022-10-13 20:41:47 +0530

    project

---
+++ backend/data/products.js
@@ -0,0 +1,100 @@
+const products = [
+  {
+    name: `Almond Granola`,
+    image: '/images/almond-granola.jpg',
+    description:
+      'Homemade granola with: almonds, cashews, hazelnuts, walnuts, chia seeds, flax seeds, sunflower seeds, pumpkin seed, plums, blueberries and oats. No added flavors, no added sugar or preservatives.',
+    brand: 'Granola',
+    category: 'Food',
+    price: 2499.99,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 6,
+  },
+  {
+    name: 'Black Garlic Peeled Cloves',
+    image: '/images/black-garlic.jpg',
+    description:
+      'Black Garlic is made when whole bulbs of garlic are expertly aged in controlled heat and humid condition for several weeks. Black Garlic is 100% natural, loaded with Anti-oxidants and has many other health benefits too. It is a well known gourmet ingredient so can be used on many recipes or can be enjoyed on its own as a healthy snack. Black Garlic tastes nothing like garlic; it is sweet, tangy and mild umami flavor.',
+    brand: 'Ceylon Black Garlic',
+    category: 'Vegan & Vegetarian',
+    price: 1399.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    name: 'Classic Badhuma',
+    image: '/images/baduma.jpg',
+    description:
+      'Badhuma is a mix of locally sourced ingredients which are mixed together with local spices and karapincha to give an extra twist to your everyday meal.',
+    brand: 'Badhuma',
+    category: 'Savory Snaks',
+    price: 2700.0,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 9,
+  },
+  {
+    name: 'Crunchy Peanut Butter',
+    image: '/images/peanut-butter.jpg',
+    description:
+      'Handcrafted Natural Crunchy Peanut Butter, made with organic roasted peanut, organic extra virgin coconut oil, organic coconut sugar, Himalayan pink salt. 100% natural and vegan. No nasties.',
+    brand: "Peas & Love's",
+    category: 'Food, Nut & Seed Butter',
+    price: 990.0,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 7,
+  },
+  {
+    name: 'Date & Tamarind Chutney',
+    image: '/images/Chutney.jpg',
+    description:
+      'This range of chutneys are made with the date fruit carefully sourced for freshness. Home made with love, we like to give you what we call “a taste of home”. Ingredients: Dates, Sugar, Vinegar Tamarind, Garlic, Ginger',
+    brand: 'Date Bar',
+    category: 'Dips & Sauses',
+    price: 1299.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 8,
+  },
+  {
+    name: 'Coconut Chip - Roasted',
+    image: '/images/chip.jpg',
+    description:
+      'Satiable nutritious food is the way to one’s heart and soul. We re-awaken Ceylon’s finest elements and edibles for a healthy and conscious lifestyle.',
+    brand: 'Nutritive',
+    category: 'Health Food',
+    price: 280.0,
+    countInStock: 5,
+    rating: 4,
+    numReviews: 5,
+  },
+  {
+    name: 'Ceylon Classic Tea',
+    image: '/images/tea.jpg',
+    description:
+      'Uplift your teatime with Ceylon classic tea. This pure, single-origin Ceylon tea is grown in the central highlands of Sri Lanka. Its distinctive aroma and flavour are then carefully enhanced by experienced master blender through time-honored practices, bringing you a sublime experience. Perfectly complemented with milk or a slice of lemon. Comes with 25 envelopes.',
+    brand: 'Truly Ceylon',
+    category: 'Tea, Herbal Drinks & Coffee',
+    price: 550.0,
+    countInStock: 6,
+    rating: 4,
+    numReviews: 5,
+  },
+  {
+    name: 'Holiday Fruitcake Tea',
+    image: '/images/cake.jpg',
+    description:
+      'A blend of Japanese Style Sencha Organic Green Tea, Hibiscus, Rosehip and Rosepetals, 25 servings.',
+    brand: 'Ceylon ME Tea',
+    category: 'Tea, Herbal Drinks & Coffee',
+    price: 899.99,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 5,
+  },
+];
+
+export default products;
